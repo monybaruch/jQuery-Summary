f01237fd1a17c5e5f19fdaaf8a344173983531e0
מוני ברוך <monybaruch@gmail.com>
2023-03-10 15:29:02 +0200

    starter files

---
+++ index.js
@@ -0,0 +1,13 @@
+// // $(document).ready(function () {
+// //   $("h1").css("color", "red");
+// // });
+// document.querySelector("h1"); //js
+
+// $("h1"); //jQuery
+
+// adding a jQuery class to css element
+
+$("h1").addClass("big-title");
+
+//removing a jQuery class
+$("h1").removeClass("big-title");
